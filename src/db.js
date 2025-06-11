3298156a5ce1486e4bc2c31307f6efa661e731b7
susheel1606 <susheelyadav2556@gmail.com>
2025-06-12 00:44:09 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,10 +8,9 @@ const pool = new Pool({
     database: process.env.DB_NAME || 'url_shortener',
     password: process.env.DB_PASSWORD || 'password',
     port: process.env.DB_PORT || 5432,
-    ssl: false
-    // ssl: {
-    //     rejectUnauthorized: false // Allow self-signed AWS certificates
-    // }
+    ssl: {
+        rejectUnauthorized: false // Allow self-signed AWS certificates
+    }
 })
 
 // Test the database connection
