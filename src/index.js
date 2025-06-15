9d89e99f61cecb0b95b4d432d3943c72e9ff0d6a
susheel1606 <susheelyadav2556@gmail.com>
2025-06-15 16:18:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.use(cors())
 app.use(router)
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`))
 
 module.exports = app
